7d03e02f0fbd49a092ac5c62e8c63737f274b959
ylxdzsw <ylxdzsw@gmail.com>
2016-06-26 17:55:21 +0800

    update

--- src/OhMyJulia.jl
+++ src/OhMyJulia.jl
@@ -1,3 +1,5 @@
+__precompile__()
+
 module OhMyJulia
 
 include("abbr.jl")
--- src/io.jl
+++ src/io.jl
@@ -3,8 +3,14 @@ import Base: <<, >>
 <<(x::IO, y::Int) = (write(x, y); x) # can be removed if Base fixed
 <<(x::IO, y::Integer) = (write(x, y); x) # can be removed if Base fixed
 <<(x::IO, y) = (write(x, y); x)
-<<(x::IO, f::Function) = f(x)
+<<(x::IO, f::Function) = (f(x); x)
 
->>(x::IO, y::Int) = read(x, y) # can be removed if Base fixed
->>(x::IO, y::Integer) = read(x, y) # can be removed if Base fixed
+>>(x::IO, y::Int) = readbytes(x, y) # can be removed if Base fixed
+>>(x::IO, y::Integer) = readbytes(x, y)
 >>(x::IO, y) = read(x, y)
+>>(x::IO, f::Function) = f(x)
+
+>>>(x::IO, y::Int) = (readbytes(x, y); x) # can be removed if Base fixed
+>>>(x::IO, y::Integer) = (readbytes(x, y); x)
+>>>(x::IO, y) = (read(x, y); x)
+>>>(x::IO, f::Function) = (f(x); x)
