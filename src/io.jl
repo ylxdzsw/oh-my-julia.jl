e4ec2ba9c08186248ccdb0c26092b773f2a717b3
ylxdzsw <ylxdzsw@gmail.com>
2016-06-26 16:43:09 +0800

    fix ambiguous methods

--- src/io.jl
+++ src/io.jl
@@ -1,5 +1,10 @@
 import Base: <<, >>
 
+<<(x::IO, y::Int) = (write(x, y); x) # can be removed if Base fixed
+<<(x::IO, y::Integer) = (write(x, y); x) # can be removed if Base fixed
 <<(x::IO, y) = (write(x, y); x)
 <<(x::IO, f::Function) = f(x)
+
+>>(x::IO, y::Int) = read(x, y) # can be removed if Base fixed
+>>(x::IO, y::Integer) = read(x, y) # can be removed if Base fixed
 >>(x::IO, y) = read(x, y)
