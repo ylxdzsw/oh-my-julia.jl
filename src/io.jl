9c3cfba6c970d84990ab814c6ecc425428a44173
ylxdzsw <ylxdzsw@gmail.com>
2016-06-27 11:50:50 +0800

    <<

--- src/io.jl
+++ src/io.jl
@@ -1,8 +1,10 @@
-import Base: <<, >>
+import Base: <<, >>, >>>
 
-<<(x::IO, y::Int) = (write(x, y); x) # can be removed if Base fixed
-<<(x::IO, y::Integer) = (write(x, y); x) # can be removed if Base fixed
-<<(x::IO, y) = (write(x, y); x)
+<<(x::IO, y::Int) = (print(x, y); x) # can be removed if Base fixed
+<<(x::IO, y::Integer) = (print(x, y); x) # can be removed if Base fixed
+<<(x::IO, y) = (print(x, y); x)
+<<(x::IO, y::Byte) = (write(x, y); x)
+<<(x::IO, y::Bytes) = (write(x, y); x)
 <<(x::IO, f::Function) = (f(x); x)
 
 >>(x::IO, y::Int) = readbytes(x, y) # can be removed if Base fixed
