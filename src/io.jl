bf401df54cbe91e1ed2a9b32635876998290c299
ylxdzsw <ylxdzsw@gmail.com>
2016-08-26 11:35:04 +0800

    update io for julia 0.5

--- src/io.jl
+++ src/io.jl
@@ -1,18 +1,14 @@
 import Base: <<, >>, >>>
 
-<<(x::IO, y::Int) = (print(x, y); x) # can be removed if Base fixed
-<<(x::IO, y::Integer) = (print(x, y); x) # can be removed if Base fixed
 <<(x::IO, y) = (print(x, y); x)
 <<(x::IO, y::Byte) = (write(x, y); x)
 <<(x::IO, y::Bytes) = (write(x, y); x)
 <<(x::IO, f::Function) = (f(x); x)
 
->>(x::IO, y::Int) = readbytes(x, y) # can be removed if Base fixed
->>(x::IO, y::Integer) = readbytes(x, y)
+>>(x::IO, y::Integer) = read(x, y)
 >>(x::IO, y) = read(x, y)
 >>(x::IO, f::Function) = f(x)
 
->>>(x::IO, y::Int) = (readbytes(x, y); x) # can be removed if Base fixed
->>>(x::IO, y::Integer) = (readbytes(x, y); x)
+>>>(x::IO, y::Integer) = (read(x, y); x)
 >>>(x::IO, y) = (read(x, y); x)
 >>>(x::IO, f::Function) = (f(x); x)
