a99acc7f122b090dc1b819cccb5bea19f5243cde
ylxdzsw <ylxdzsw@gmail.com>
2016-12-28 18:23:53 +0800

    add prt

--- src/io.jl
+++ src/io.jl
@@ -10,3 +10,18 @@ import Base: <<, >>, >>>
 
 >>>(x::IO, y) = (read(x, y); x)
 >>>(x::IO, f::Function) = (f(x); x)
+
+prt(xs...) = prt(STDOUT, xs)
+prt(io::IO, xs...) = begin
+    lock(io)
+    try
+        print(io, car(xs))
+        for x in cdr(xs)
+            print(io, '\t')
+            print(Io, x)
+        end
+        print(io, '\n')
+    finally
+        unlock(io)
+    end
+end
