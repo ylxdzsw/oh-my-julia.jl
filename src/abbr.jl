8cfa4414f5dd7340fca1909960e7d4208ea392a7
ylxdzsw <ylxdzsw@gmail.com>
2016-12-29 09:03:35 +0800

    add type alias

--- src/abbr.jl
+++ src/abbr.jl
@@ -1,7 +1,15 @@
-export ∞, Bytes, Byte
+export ∞, Bytes, Byte, u8, u16, u32, u64, i32, i64, f32, f64
 
 const ∞ = Inf
 
 typealias Bytes Vector{UInt8}
 typealias Byte UInt8
-typealias AbstractBytes AbstractArray{Byte, 1}
+
+typealias u8  UInt8
+typealias u16 UInt16
+typealias u32 UInt32
+typealias u64 UInt64
+typealias i32 Int32
+typealias i64 Int64
+typealias f32 Float32
+typealias f64 Float64
