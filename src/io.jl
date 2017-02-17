5f163e83610b68ff640ae618e36e529a7838549f
ylxdzsw <ylxdzsw@gmail.com>
2017-02-17 17:57:25 +0800

    revert behaviour of io << usigned

--- src/io.jl
+++ src/io.jl
@@ -1,8 +1,8 @@
 import Base: <<, >>, >>>
 
 <<(x::IO, y) = (print(x, y); x)
+<<(x::IO, y::Byte) = (write(x, y); x)
 <<(x::IO, y::Bytes) = (write(x, y); x)
-<<(x::IO, y::Unsigned) = (write(x, y); x)
 <<(x::IO, f::Function) = (f(x); x)
 
 >>(x::IO, y) = read(x, y)
