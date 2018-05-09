75b82f76771d1703ad0def30363803233a79c691
ylxdzsw <ylxdzsw@gmail.com>
2018-05-09 10:57:28 +0800

    add cons

--- src/foreign.jl
+++ src/foreign.jl
@@ -1,10 +1,15 @@
-export car, cdr, cadr,
+export car, cdr, cadr, cons
        nrow, ncol, void
 
 @inline car(x::ANY)   = x[1]
 @inline cdr(x::ANY)   = x[2:end]
 @inline cdr(x::Tuple) = Base.tail(x)
 @inline cadr(x::ANY)  = x[2]
+
+@inline cons(x::Tuple, y::ANY) = (x..., y)
+@inline cons(x::ANY, y::Tuple) = (x, y...)
+@inline cons(x::Tuple, y::Tuple) = (x..., y...)
+@inline cons(x::ANY, y::ANY) = (x, y)
 
 @inline nrow(x::ANY) = size(x,1)
 @inline ncol(x::ANY) = size(x,2)
