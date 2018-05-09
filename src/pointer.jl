70c0f7153a62b18c0abdf9e83d18e9dba6c7024f
ylxdzsw <ylxdzsw@gmail.com>
2018-05-09 16:23:27 +0800

    yet another fancy representation

--- src/pointer.jl
+++ src/pointer.jl
@@ -2,3 +2,7 @@ import Base: getindex, setindex!
 
 getindex(p::Ptr, i::Integer=1) = unsafe_load(p, i)
 setindex!(p::Ptr, x, i::Integer=1) = unsafe_store!(p, x, i)
+
+import Base: *
+
+*(::Type{T}) where T = Ptr{T}
